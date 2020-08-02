13ae15ec6f4ce896e89e812b2049fc55cb2d58e1
Josh Raiborde <joshsrai@yahoo.com>
2020-08-02 01:17:12 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,8 @@
-class Dog
- 
-  def name
-    @name
+class Dog 
+  def name=(dogs_name)
+    @this_dogs_name = dogs_name
   end
- 
-  def name=(name)
-    @name = name
+  def name 
+    @this_dogs_name
   end
- 
 end
